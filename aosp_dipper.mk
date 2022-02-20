87271c110f21ffefe633ebd7a2264b82d68c4f20
namaskaragusthi <senaoutsider123@gmail.com>
2022-02-21 02:16:30 +0530

    dipper: Update Fingerprint to Raven:12/SQ1D.220205.003

--- aosp_dipper.mk
+++ aosp_dipper.mk
@@ -27,10 +27,10 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 8
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "Xiaomi/dipper/dipper:8.1.0/OPM1.171019.011/V9.5.5.0.OEAMIFA:user/release-keys"
+BUILD_FINGERPRINT := "google/raven/raven:12/SQ1D.220205.003/8069835:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="dipper-user 8.1.0 OPM1.171019.011 V9.5.5.0.OEAMIFA release-keys" \
+    PRIVATE_BUILD_DESC="raven-user 12 SQ1D.220205.003 8069835 release-keys" \
     PRODUCT_NAME="dipper"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
